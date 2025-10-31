8a31a416aabc145e3e668e65e7e53532ff63df5d
berndtjohannsen <berndtjohannsen@yahoo.com>
2025-10-31 17:23:38 +0100

    Update application version to 1.0.349, enhance install button functionality in index.html, and modify SVG path for improved visual representation.

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
  */
 
 // Application version - Update this when releasing new versions
-export const APP_VERSION = '1.0.348';
+export const APP_VERSION = '1.0.349';
 
 // Application name
 export const APP_NAME = 'Tiddeli PWA Template';
